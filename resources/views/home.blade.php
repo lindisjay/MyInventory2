b5d845d9bd9bfffe530ca78ce6a7de5d51152b77
lindisjay <lindisrijayanti3771@gmail.com>
2022-10-17 08:45:42 +0700

    inventory system

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <center><h1 style="font-family:Roboto;"><strong >Selamat Datang di Sistem Inventaris </strong></h1></center>
+                    <br><center><h3 style="font-family:Roboto">  Batik Shop Ambar</h3></center>
+                </div>
+                <div class="panel-body">
+                @if(Auth::user()->jabatan == 'ADMIN')
+                    <div class="panel-body" align="center">
+                        <center><img src="images/stok.jpg" alt="" width="500px" height="500px"></center>
+                        <!--<div>
+                            <a href="{{ route('category.index') }}"><img title="kategori barang" src="images/category.jfif" alt="kategori barang"></a>                     
+                            <a href="{{ route('supplier.index') }}"><img title="daftar supplier" src="images/supplier.png"></a>
+                            <a href="{{ route('customer.index') }}"><img title="daftar customer" src="images/customer.jpg"></a>
+                        </div>
+                        <div><hr></div>
+                        <div>
+                            <a href="{{ route('product.index') }}"><img title="daftar barang" src="images/product.png"></a>
+                            <a href="{{ route('sell.index') }}"><img title="penjualan" src="images/sell.jfif"></a>
+                            <a href="{{ route('purchase.index') }}"><img title="pembelian" src="images/purchase.jpg"></a>
+                        </div>-->
+                    </div>
+                @else
+                    <div class="panel-body">
+                        <center><img src="images/ambar.jpg" alt="" width="500px" height="500px"></center>
+                    </div>
+                @endif  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
